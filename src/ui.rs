e6306079313f9702b76c4c799be87314750e2226
stephen <stephen.buchanan@random.studio>
2023-12-19 15:33:43 +0100

    allow scroll channels

--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,17 @@
-use egui::{Slider, Ui};
+use egui::{ScrollArea, Slider, Ui};
 
 use crate::{model::Model, settings::CHANNELS_PER_UNIVERSE};
 
 pub fn render(model: &mut Model, ui: &mut Ui) {
     ui.heading("Tether ArtNet");
-    for i in 0..CHANNELS_PER_UNIVERSE {
-        ui.add(Slider::new(&mut model.channels_state[i as usize], 0..=255));
-    }
+    ScrollArea::vertical()
+        .auto_shrink([false, false])
+        .show(ui, |ui| {
+            for i in 0..CHANNELS_PER_UNIVERSE {
+                ui.horizontal(|ui| {
+                    ui.label(format!("Channel #{}", i + 1));
+                    ui.add(Slider::new(&mut model.channels_state[i as usize], 0..=255));
+                });
+            }
+        });
 }
